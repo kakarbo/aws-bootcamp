b0d2eb0d5bbcd4dc67a3196e81955730d43a6d4f
Cristhian Carbonell <alexcarbo8809@gmail.com>
2025-06-10 21:56:24 +0000

    change function decoration flask

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -85,7 +85,7 @@ cors = CORS(
 #     return response
 
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
-@app.before_first_request
+@app.before_request
 def init_rollbar():
     """init rollbar module"""
     rollbar.init(
